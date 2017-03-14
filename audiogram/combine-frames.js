0fca11e578a68dae6e42f8c1e3cf0fa5f8d3f33f
Jonty Usborne <jonty.usborne@bbc.co.uk>
2017-03-14 11:17:04 +0000

    Use spawn not exec during combineFrames

--- audiogram/combine-frames.js
+++ audiogram/combine-frames.js
@@ -1,12 +1,31 @@
-var exec = require("child_process").exec;
-
 function combineFrames(options, cb) {
 
+  function run(args, callback) {
+    var err;
+    var spawn = require("child_process").spawn;
+    var command = spawn("ffmpeg", args);
+    command.stderr.on('data', function(data) {
+      err = "combineFrames - ffmpeg error: " + data;
+    });
+    command.on('exit', function() {
+      return callback(err);
+    });
+  }
+  
   // Raw ffmpeg command with standard mp4 setup
   // Some old versions of ffmpeg require -strict for the aac codec
-  var cmd = "ffmpeg -r " + options.framesPerSecond + " -i \"" + options.framePath + "\" -i \"" + options.audioPath + "\" -c:v libx264 -c:a aac -strict experimental -shortest -pix_fmt yuv420p \"" + options.videoPath + "\"";
-
-  exec(cmd, cb);
+  var args = [
+    '-loglevel', 'error',
+    '-r', options.framesPerSecond,
+    '-i', options.framePath,
+    '-i', options.audioPath,
+    '-c:v', 'libx264',
+    '-c:a', 'aac',
+    '-strict', 'experimental',
+    '-shortest',
+    '-pix_fmt', 'yuv420p', options.videoPath
+  ];
+  run(args, function(err){cb(err)});
 
 }
 

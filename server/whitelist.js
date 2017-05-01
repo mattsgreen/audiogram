2d4dfce8994c898c49f5c3cc6350fbd0a2633e2b
Jonty Usborne <jonty.usborne@bbc.co.uk>
2017-05-01 18:23:14 +0100

    Return changes to whitelist

--- server/whitelist.js
+++ server/whitelist.js
@@ -15,20 +15,31 @@ function set(req, res) {
 	var fs = require('fs'),
 		path = require("path"),
 		listCurrent = require('../whitelist.json'),
-		listNew = req.body.list.split("\r\n");
-	for (var person in listNew) {
-		if ( listNew.hasOwnProperty(person) && listCurrent.indexOf(listNew[person]) === -1 ) {
-			console.log("New user >>> " + listNew[person]);
+		listNew = req.body.list.replace("\r","").split("\n"),
+		diff = {"added":[],"removed":[]};
+	// Find newly added users
+		for (var person in listNew) {
+			if ( listNew.hasOwnProperty(person) && listCurrent.indexOf(listNew[person]) === -1 ) {
+				diff.added.push(listNew[person]);
+			}
 		}
-	}
-	var list = JSON.stringify(listNew);
-	fs.writeFile(path.join(__dirname, "../whitelist.json"), list, function(err){
-		if (err) {
-			return res.json({error: err});
-		} else {
-			return editor(req, res);
+	// Find removed users
+		for (var person in listCurrent) {
+			if ( listCurrent.hasOwnProperty(person) && listNew.indexOf(listCurrent[person]) === -1 ) {
+				diff.removed.push(listCurrent[person]);
+			}
 		}
-	});
+	console.log(JSON.stringify(diff));
+	// Update list
+		var list = JSON.stringify(listNew);
+		fs.writeFile(path.join(__dirname, "../whitelist.json"), list, function(err){
+			if (err) {
+				return res.json({error: err});
+			} else {
+				return res.json({error: null, diff: diff});
+				// return editor(req, res);
+			}
+		});
 }
 
 module.exports = {
